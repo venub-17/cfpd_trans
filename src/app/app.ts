baf108e6a3afa5df10c5ca2a14218a8616f7575a
venub34 <venubeenaveni1734@gmail.com>
2025-08-19 18:32:16 -0400

    initial push

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { ModalComponent } from './shared/modal-component/modal-component';
+import { LoaderComponent } from './shared/loader-component/loader-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, RouterOutlet, ModalComponent, LoaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('cfpd-transformer');
+  protected readonly title = signal('transformer');
 }
