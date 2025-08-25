801a8812a1e5d26e89cbbeb5e96b5f1098dec9b6
venub34 <venubeenaveni1734@gmail.com>
2025-08-25 13:42:54 -0400

    worked on dropdown

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Service } from '../../shared/types';
+import { serviceData } from '../../shared/temp/service.data';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +12,17 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Header implements OnInit {
   isLoggedIn: boolean = false;
+  openDropdown: string = '';
+  services: Service[] = [];
   ngOnInit(): void {}
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.services = serviceData;
+  }
   toggleState: boolean = false;
   onToggleMenu() {
     this.toggleState = !this.toggleState;
+  }
+  onToggleDropdown(name: string) {
+    this.openDropdown = name ? name : '';
   }
 }
