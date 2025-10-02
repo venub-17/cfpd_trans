618d2c5a485ce401bcf2a63854fa5b567e300f86
venub34 <venubeenaveni1734@gmail.com>
2025-10-01 21:24:50 -0400

    Added assistance in contact

--- src/app/components/contacts/contacts.ts
+++ src/app/components/contacts/contacts.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../../shared/services/modal.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contacts.html',
   styleUrl: './contacts.scss',
 })
 export class Contacts {
+  showAssitanceModal: boolean = false;
   constructor(private modalService: ModalService) {}
   onOpenModal() {
     this.modalService.open();
@@ -25,5 +27,12 @@ export class Contacts {
       content: 'Details about the contact will be displayed here.',
       isConfirmButton: true,
     });
+  }
+  onAssitance() {
+    console.log('Assistance clicked');
+    this.showAssitanceModal = true;
+  }
+  closeAssitanceModal() {
+    this.showAssitanceModal = false;
   }
 }
