59dee3c8de5e1a389f88dcee3b7efcc219827219
venub34 <venubeenaveni1734@gmail.com>
2025-10-12 16:00:46 -0400

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  // apiUrl: 'https://cfpapihosted-production-9af9.up.railway.app',
+  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://cfpapihosted-production-9af9.up.railway.app',
 };
