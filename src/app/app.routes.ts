b3c8173ed208d2e924a51f3236caad188d778c16
venub34 <venubeenaveni1734@gmail.com>
2025-10-13 15:10:36 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -53,10 +53,10 @@ export const routes: Routes = [
     component: Signup,
   },
   {
-    path: 'reset-password',
+    path: 'reset-password/:token',
     loadComponent: () =>
       import('./components/auth/resetpwd/resetpwd').then((m) => m.Resetpwd),
-    // canActivate: [ResetTokenGuard],
+    canActivate: [ResetTokenGuard],
   },
   {
     path: 'forgot-password',
